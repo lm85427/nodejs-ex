a2a26821f307184deff96e43557a70af3075f79d
LM85427 <liam.murphy@lgim.com>
2020-11-20 10:53:59 +0000

    fleshing out form

--- server.js
+++ server.js
@@ -71,8 +71,8 @@ var initDb = function(callback) {
 };
 
 app.get('/', function (req, res) {
-    //res.render('index.html', { pageCountMessage : null});
-  res.render(reg_form.toHTML());
+  //res.render('index.html', { pageCountMessage : null});
+  res.render('index.html', { form : reg_form.toHTML()});
 });
 
 // error handling
